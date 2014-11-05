ef6c873e1dd11d86ee0bad58a8c14fec2d255562
Mario Hsieh <mariohsieh@gmail.com>
2014-11-04 22:15:48 -0800

    changed to express 4, added bower

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 // dependencies //
-var express = require('express'),
-	app = express(),
-	http = require('http').Server(app),
-	io = require('socket.io')(http);
-	
+var express 				= require('express'),
+		app 						= express(),
+		morgan					= require('morgan'),
+		bodyParser			= require('body-parser'),
+		methodOverride	= require('method-override'),
+		http 						= require('http').Server(app),
+		io 							= require('socket.io')(http);
+
+
 // configuration //
 var port = Number(process.env.PORT || 9099);
-app.configure(function() {
-	app.set('view engine', 'html');
-	app.set('views', __dirname + '/public/views');
-	app.use(express.logger('dev'));
-	app.use(express.static(__dirname + '/public'));
-	
-});
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
 
 // routes //
 app.get('/', function(req,res) {
 	res.sendfile('./public/index.html');
 });
 
+
 // socket.io //
 //require('./io')(app,io);
 
+
 // open server //
-app.listen(port);
-console.log("App running on localhost:9099");
+app.listen(port, function(err) {
+	if (err) throw err;
+	console.log("App running on localhost: " + port);
+});
